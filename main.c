56d5c6a64e6cf4888b85ab8c875f875ad152b3e1
Miller <jm789016@ohio.edu>
2018-10-25 18:51:06 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_CHARS 60
+
+
+int main()
+{
+
+    printf("Enter some text: ");
+
+    char enteredText[MAX_CHARS];
+    gets(enteredText);
+
+    int a; //this will navigate through the array.
+    int digitCounter = 0;
+
+    for (a = 0; a < MAX_CHARS; a++)
+    {
+        int currentChar = enteredText[a];
+
+        if ( isdigit(currentChar) )
+        {
+            digitCounter++;
+
+        }
+
+    }
+
+
+    printf("\nEncountered %d digits. \n", digitCounter);
+
+    return 0;
+}
